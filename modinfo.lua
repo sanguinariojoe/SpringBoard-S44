17fbdabe0be47dbcb159a631f1ec112d3b9a3c3a
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2017-08-29 13:11:54 +0200

    Set in test mode

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,14 @@
 return {
-  name='Scenario Editor S44',
+  name='SpringBoard S44',
   description='Scenario Editor for Spring: 1944',
   shortname='SE S44',
   version='git',
   mutator='Official',
-  game='Scenario Editor S44',
+  game='SpringBoard S44',
   shortGame='SE S44',
   modtype=1,
   depend = {  			
   	'Spring: 1944 $VERSION',
-  	'Scenario Editor Core git',
+  	'SpringBoard Core $VERSION',
   },
 }
