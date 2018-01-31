ab8a820cdc2a4874c3c263e393b1470d7184591e
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2018-01-31 14:58:44 +0100

    Typo correction on the mod name

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,11 @@
 return {
   name='SpringBoard S44',
   description='Scenario Editor for Spring: 1944',
-  shortname='SE S44',
+  shortname='SB-S44',
   version='$VERSION',
   mutator='Official',
   game='SpringBoard S44',
-  shortGame='SE S44',
+  shortGame='SB-S44',
   modtype=1,
   depend = {  			
   	'Spring: 1944 $VERSION',
